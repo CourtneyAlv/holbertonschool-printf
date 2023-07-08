9273067ec8e438dfbf03b774f9aa3e89cff133dc
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-07 18:23:21 -0700

    Removed prototypes, redunant

--- main.h
+++ main.h
@@ -9,9 +9,6 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _atoi(char *s);
-void va_start(va_list ap, last);       
-type va_arg(va_list ap, type);
-void va_end(va_list ap);
-void va_copy(va_list dest, va_list src);
+       
 
 #endif
