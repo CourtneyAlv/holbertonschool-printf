8910800240cc4eebeb4022c99fbb479fdbaf0a55
TPoe25 <t_poe25@yahoo.com>
2023-07-07 21:08:52 -0700

    string file updated

--- prints.c
+++ prints.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+void _puts(char *str)
+{
+	int place;
+
+	for (place = 0; *(str + place) != '\0'; place++)
+	{
+		_putchar(*(str + place));
+	}
+	_putchar('\n');
+}
+int _printf(const char *format, ...)
+{
+    _puts(*format);
+    return 0;
+}
