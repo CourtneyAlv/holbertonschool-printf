4ab8603ad93493b5646794d60df17d95404be5d6
TPoe25 <t_poe25@yahoo.com>
2023-07-10 16:03:42 -0700

    updates

--- main.h
+++ main.h
@@ -14,9 +14,9 @@ int _atoi(char *s);
 int _printch(va_list args);       
 int _printf(const char *format, ...);
 int _percent(va_list args);
-int print_string(va_list args);
+int int_to_str(char *buffer, int num);
+int print_int_helper(int num);
 int print_int_d(va_list args);
 int print_int_i(va_list args);
-int int_to_str(char *buffer, int num);
 
 #endif
---
+++ newinterger.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int print_int_helper(int num)
+{
+	char digit = '0' + (num % 10);
+	int count = 0;
+	
+	if (num == 0)
+       	{
+		write(1, "0", sizeof(char));
+		return 1;
+	}
+	if (num / 10 != 0) 
+	{
+		count += print_int_helper(num / 10);
+	}
+	write(1, &digit, sizeof(char));
+	return (count + 1);
+}
+
+int print_int_d(va_list args)
+{	
+	int num = va_arg(args, int);
+        int count = 0;
+	
+	if (num < 0)
+       	{
+        	write(1, "-", sizeof(char));
+        	num = -num;
+        	count++;
+	}
+       	count += print_int_helper(num);
+	return (count);
+}
+int print_int_i(va_list args)
+{
+	int num = va_arg(args, int);
+	return (print_int_helper(num));
+}
